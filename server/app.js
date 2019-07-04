9dbd279e1c297a25cb701c16249b64b0dce80b0e
Corneliu <corneliu.gaina@gmail.com>
2019-07-04 18:09:26 +0200

    express app setup

---
+++ server/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql'); // création du serveur express qui va executer graphql api
+
+const app = express();
+
+app.use('/graphql', graphqlHTTP({ // fonction servant de noeud central/middleware qui va envoyer toutes les requetes graphql en un seul endroit
+
+}))
+
+
+
+app.listen(4000, () => {
+    console.log('now listening for request on port 4000');
+})
